2bcac6377cacce93beeb037f99086b17fd6ac1dc
Chris Dolphin <dolphin@likethemammal.com>
2019-12-26 23:23:28 -0800

    Add support for boolean values

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function theme(name, values) {
 
 theme.variants = function(name, prop, values) {
   return function(props) {
-    var variant = props[prop] && values[props[prop]];
+    var variant = typeof props[prop] !== 'undefined' && values[props[prop]];
     return variant && getThemeValue(name, props, variant);
   };
 };
